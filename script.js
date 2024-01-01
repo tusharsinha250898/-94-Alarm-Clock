ec5006ccd1ce0fc1f4f60adcca74664751c0c856
tusharsinha2550898 <tusharsinha721@gmail.com>
2024-01-01 16:24:29 +0530

    Lion roar sound

---
+++ script.js
@@ -0,0 +1,15 @@
+// function play() {
+//     var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+//     audio.play();
+// }
+// setTimeout(() => {
+//     play()
+// }, 2000)
+
+function play() {
+    var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+    audio.play();
+}
+setTimeout(() => {
+    play()
+}, 2000)
